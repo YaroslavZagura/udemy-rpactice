7779fa4fa3c631b93b45ceb6a316f080804e22fe
Yaroslav <ya.sahurskyi@gmail.com>
2023-08-10 13:20:09 +0300

    created questions about films and obj movie

--- program/js/script.js
+++ program/js/script.js
@@ -7,3 +7,13 @@ let personalMovieDB = {
     genres: [],
     privat: false
 };
+
+let firstOfLastMovie = prompt("Один з останніх переглянутих фільмів?", ""),
+    ratingFirstOfLastMovie = prompt("На скільки його оціните?", "від 1 до 10"),
+    secondOfLastMovie = prompt("Один з останніх переглянутих фільмів?", ""),
+    ratingSecondOfLastMovie = prompt("На скільки його оціните?", "від 1 до 10");
+
+personalMovieDB.movies[firstOfLastMovie] = ratingFirstOfLastMovie;
+personalMovieDB.movies[secondOfLastMovie] = ratingSecondOfLastMovie;
+
+console.log(personalMovieDB);
