e69ec95b35da78e0855f9f762015fb17f5fef5c1
Yaroslav <ya.sahurskyi@gmail.com>
2023-08-10 13:17:10 +0300

    created obj personalMovieDB

--- program/js/script.js
+++ program/js/script.js
@@ -1,1 +1,9 @@
 let numberOfFilms = prompt("Скільки фільмів ви вже переглянули?", "");
+
+let personalMovieDB = {
+    count: +numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
