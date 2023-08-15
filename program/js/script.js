41aafa61b47cd7acfd44be81dd60502b36918aa6
Yaroslav <ya.sahurskyi@gmail.com>
2023-08-15 17:14:02 +0300

    first update questin

--- program/js/script.js
+++ program/js/script.js
@@ -8,12 +8,27 @@ let personalMovieDB = {
     privat: false
 };
 
-let firstOfLastMovie = prompt("Один з останніх переглянутих фільмів?", ""),
-    ratingFirstOfLastMovie = prompt("На скільки його оціните?", "від 1 до 10"),
-    secondOfLastMovie = prompt("Один з останніх переглянутих фільмів?", ""),
-    ratingSecondOfLastMovie = prompt("На скільки його оціните?", "від 1 до 10");
+// let firstOfLastMovie = prompt("Один з останніх переглянутих фільмів?", ""),
+//     ratingFirstOfLastMovie = prompt("На скільки його оціните?", "від 1 до 10"),
+//     secondOfLastMovie = prompt("Один з останніх переглянутих фільмів?", ""),
+//     ratingSecondOfLastMovie = prompt("На скільки його оціните?", "від 1 до 10");
 
-personalMovieDB.movies[firstOfLastMovie] = ratingFirstOfLastMovie;
-personalMovieDB.movies[secondOfLastMovie] = ratingSecondOfLastMovie;
+// personalMovieDB.movies[firstOfLastMovie] = ratingFirstOfLastMovie;
+// personalMovieDB.movies[secondOfLastMovie] = ratingSecondOfLastMovie;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// мій варіант№1
+for (let i = 0; i < 2; i++) {
+    let oneOfLastMovie = prompt("Один з останніх переглянутих фільмів?", "");
+    let ratingOneOfLastMovie = prompt("На скільки його оціните?", "від 1 до 10");
+
+    if (oneOfLastMovie === "" || ratingOneOfLastMovie === "" || oneOfLastMovie === null || ratingOneOfLastMovie === null || oneOfLastMovie.length > 50) {
+        i--;
+        console.log("error");
+        continue;
+    };
+
+    console.log("done");
+    personalMovieDB.movies[oneOfLastMovie] = ratingOneOfLastMovie;
+}
